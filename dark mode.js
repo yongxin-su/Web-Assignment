57dd3d2e2fba74719f401596036ee27f7725be79
yongxin-su <bernicesiew23@gmail.com>
2026-07-25 23:30:11 +0800

    Update dark mode.js

--- dark mode.js
+++ dark mode.js
@@ -1,29 +1,29 @@
 
-const use_theme = 'user-theme';
+const use_theme = 'user-theme'; //save the theme
 
 function applyTheme(theme) {
   if (theme === 'dark') {
-    document.documentElement.classList.add('dark');
+   document.documentElement.setAttribute('dark-theme', 'dark');
   } else {
-    document.documentElement.classList.remove('dark');
+    document.documentElement.removeAttribute('dark-theme');
   }
-}
+} //check whether theme is dark, if yes then change everything to dark-thme
 
 const savedTheme = localStorage.getItem(use_theme);
 const darktheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
 const initialTheme = savedTheme || (darktheme ? 'dark' : 'light');
 
-applyTheme(initialTheme);
+applyTheme(initialTheme); // load page wiht light thme
 
 window.addEventListener('storage', (e) => {
   if (e.key === use_theme) {
     applyTheme(e.newValue);
   }
-});
+}); //sync
 
 function toggle() {
     
-  const currentTheme = document.documentElement.classList.contains('dark') ? 'dark' : 'light';
+  const currentTheme = document.documentElement..getAttribute('dark-theme') === 'dark' ? 'dark' : 'light';
   const newtheme = currentTheme === 'dark' ? 'light' : 'dark';
   
   localStorage.setItem(use_theme, newtheme);
